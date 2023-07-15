f80c31e95cb9dbfb8cfd6a5ad8ab0d3866bb8ed0
Tuvshuu <87944616+tuvvshe@users.noreply.github.com>
2023-07-15 12:45:35 +0200

    Add files via upload

---
+++ box_plot_using_python.py
@@ -0,0 +1,3 @@
+import pandas as pd
+data = pd.read_csv("https://artsandculture.google.com/partner/mathematisch-physikalischer-salon-royal-cabinet-of-mathematical-and-physical-instruments")
+print(data.head())
